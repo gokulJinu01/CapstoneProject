8f7ea8a5eea67a5dcc59df862654727e980ceff2
goku <goku@gokus-MacBook-Air.local>
2025-02-12 11:13:16 -0500

    backend more update

--- backEnd/server.js
+++ backEnd/server.js
@@ -24,6 +24,8 @@ app.get('/', (req, res) => {
   res.send('Hire A Chef Backend is running!');
 });
 
+// Set PORT to 3000 explicitly
+const PORT = 3000;
+
 // Start server
-const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
